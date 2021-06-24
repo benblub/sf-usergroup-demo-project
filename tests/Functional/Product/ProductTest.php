a1101d3b187f6b2131b5eb6a5400b229bda6ff7b
benjaminknecht <knecht.benjamin@yahoo.de>
2021-06-24 23:02:58 +0200

    add demo functional testclass with todos

---
+++ tests/Functional/Product/ProductTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\Functional\Product\Product;
+
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+
+class ProductTest extends ApiTestCase
+{
+    // @TODO show Basic Tests
+
+    // @TODO show Risky Tests
+    // @TODO show Risky Tests - Useless Tests
+    // @TODO show Risky Tests - Unintentionally Covered Code
+    // @TODO show Risky Tests - Output During Test Execution
+    // @TODO show Risky Tests - Test Execution Timeout
+    // @TODO show Risky Tests - Global State Manipulation
+}
