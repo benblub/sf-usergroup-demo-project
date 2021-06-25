487cc357a7f5cdaf8d14dd2afdb5af6a53692cac
benjaminknecht <knecht.benjamin@yahoo.de>
2021-06-25 08:50:43 +0200

    add risky tests

--- tests/Functional/Product/ProductTest.php
+++ tests/Functional/Product/ProductTest.php
@@ -1,18 +1,84 @@
 <?php
 
-namespace App\Tests\Functional\Product\Product;
+namespace App\Tests\Functional\Product;
 
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\Category;
+use App\Entity\Product;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductTest extends ApiTestCase
 {
     // @TODO show Basic Tests
+    public function testCreateProduct()
+    {
+        // act
+        $this->createClient()->request('POST', '/api/products', [
+            'json' => [
+                'title' => 'Salami'
+            ]
+        ]);
+
+        // assert
+        $this->assertResponseStatusCodeSame(Response::HTTP_CREATED);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertJsonContains([
+            'title' => 'Salami'
+        ]);
+    }
 
     // @TODO show Risky Tests
-    // @TODO show Risky Tests - Useless Tests
+
+    // Risky Tests - Useless Tests
+    public function testUseless()
+    {
+        // missing any assertions
+    }
+
     // @TODO show Risky Tests - Unintentionally Covered Code
-    // @TODO show Risky Tests - Output During Test Execution
-    // @TODO show Risky Tests - Test Execution Timeout
-    // @TODO show Risky Tests - Global State Manipulation
+    /**
+     * @covers Product::setTitle
+     * @TODO cant bring that test to marked as risky
+     */
+    public function testUnintentionallyCoveredCode()
+    {
+        $product = new Product();
+        $product->setTitle('foo');
+
+        $cateory = new Category();
+        $cateory->setTitle('bar');
+
+        $this->assertEquals('foo', $product->getTitle());
+    }
+
+    // Risky Tests - Output During Test Execution
+    public function testOutputDuringTestExecution()
+    {
+        var_dump('foo');
+
+        $this->assertTrue(true);
+    }
+
+    // Risky Tests - Test Execution Timeout
+    /**
+     * @small
+     */
+    public function testExecutionTimeout()
+    {
+        sleep(2);
+
+        $this->assertTrue(true);
+    }
+
+    // Risky Tests - Global State Manipulation
+    /**
+     * @backupGlobals enabled
+     */
+    public function testGlobalStateManipulation()
+    {
+        $GLOBALS['FOO'] = 'BAZ';
+
+         $this->assertTrue(true);
+    }
 }
